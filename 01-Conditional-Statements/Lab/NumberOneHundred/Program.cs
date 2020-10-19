91541954fe0702a0dc8bbb30a8f889602009c9ff
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-10-19 14:01:08 +0300

    Add files via upload

---
+++ 01-Conditional-Statements/Lab/NumberOneHundred/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace numberOneHundred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //дали е под 100, между 100 и 200 или над 200.Да се отпечатат съответно съобщения, като в примерите по - долу:
+            int number = int.Parse(Console.ReadLine());
+
+            if(number < 100)
+            {
+                Console.WriteLine("Less than 100");
+            }
+            else if(number <= 200 && number >=100)
+            {
+                Console.WriteLine("Between 100 and 200");
+            }
+            else if(number > 200)
+            {
+                Console.WriteLine("Greater than 200");
+            }
+        }
+    }
+}
