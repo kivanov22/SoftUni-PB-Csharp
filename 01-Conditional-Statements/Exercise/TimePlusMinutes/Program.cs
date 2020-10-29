9e849e3b14bee43b2effa8b487a9bdd2fc46b4a2
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-10-29 22:54:31 +0200

    Add files via upload

---
+++ 01-Conditional-Statements/Exercise/TimePlusMinutes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TimePlusMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes = minutes + 15;
+
+            if(minutes > 59)
+            {
+                hours = hours + 1;
+                minutes = minutes - 60;
+            }
+            if (hours == 24)
+            {
+                hours = 0;
+            }
+            if(minutes < 10)
+            {
+                Console.WriteLine(hours + ":0" + minutes);
+            }
+            else
+            {
+                Console.WriteLine(hours + ":" + minutes);
+            }
+        }
+    }
+}
