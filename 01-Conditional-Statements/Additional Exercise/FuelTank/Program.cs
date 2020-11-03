4f135519701fc1e0f9cce8679ae0df5be4b221e2
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-03 14:42:31 +0200

    Add files via upload

---
+++ 01-Conditional-Statements/Additional Exercise/FuelTank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FuelTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();                // lowercase sled readline na stringa , vkarvash s glavni izliza s malki
+            double number = double.Parse(Console.ReadLine());
+            
+            if(text == "gasoline" && number >= 25|| number >=25 && text =="diesel" ||number>=25 && text == "gas")  // <--- trqbva da sa s malki stringovete
+            {
+                
+                Console.WriteLine($"You have enough {text}.");
+            }
+            else if (number < 25 && text == "gasoline"||number <25 && text == "diesel"||number <25 && text == "gas")
+            {
+                
+                Console.WriteLine($"Fill your tank with {text}!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid fuel!");
+            }
+        }
+    }
+}
