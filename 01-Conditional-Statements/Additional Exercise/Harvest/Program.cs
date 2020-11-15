96092cb1757bfb3dff04a46535dabff7d6ec437d
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-15 20:14:54 +0200

    Add files via upload

---
+++ 01-Conditional-Statements/Additional Exercise/Harvest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Harvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int vineyard = int.Parse(Console.ReadLine());
+            double grapes = double.Parse(Console.ReadLine());
+            int littersWine = int.Parse(Console.ReadLine());
+            int numberWorkers = int.Parse(Console.ReadLine());
+
+            // общо грозде 
+            double totalGrape = vineyard * grapes;
+
+            //Wine
+            double wine = totalGrape * 0.40 / 2.5;
+
+            if (wine >= littersWine)
+            {
+                double litterLeft = wine - littersWine;
+                double littersPerson = litterLeft / numberWorkers;
+                Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(wine)} liters.");
+                Console.WriteLine($"{Math.Ceiling(litterLeft)} liters left -> {Math.Ceiling(littersPerson)} liters per person.");
+            }
+            else if (wine < littersWine)
+            {
+                double wineNeeded = littersWine - wine;
+                Console.WriteLine($"It will be a tough winter! More {Math.Floor(wineNeeded)} liters wine needed.");
+            }
+
+        }
+    }
+}
