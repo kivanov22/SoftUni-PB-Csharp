4db1c4efc5ce2fba5a4cca22c4751e14b53167cb
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-15 20:17:39 +0200

    Add files via upload

---
+++ 01-Conditional-Statements/Additional Exercise/PipesInPool/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace PipesInPool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int areaV = int.Parse(Console.ReadLine());
+            int p1 = int.Parse(Console.ReadLine());
+            int p2 = int.Parse(Console.ReadLine());
+            double hours = double.Parse(Console.ReadLine());
+
+            double totalP1 = p1 * hours;      
+            double totalP2 = p2 * hours;
+            double totalLitters = totalP1 + totalP2;   //общо литри
+
+            double percent = totalLitters / areaV * 100;             //проценти
+            double pipePercent1 = totalP1 / totalLitters * 100;
+            double pipePercent2 = totalP2 / totalLitters * 100;
+
+            if(totalLitters <= areaV)
+            {
+                Console.WriteLine($"The pool is {percent:f2}% full. Pipe 1: {pipePercent1:f2}%. Pipe 2: {pipePercent2:f2}%.");
+            }
+            else if (totalLitters >= areaV)
+            {
+                double overCap = totalLitters - areaV;
+                Console.WriteLine($"For {hours:f2} hours the pool overflows with {overCap} liters.");
+            }
+        }
+    }
+}
