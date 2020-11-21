34e88aa2555e710d548423740fc82c63891b9936
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-21 17:05:24 +0200

    Add files via upload

---
+++ 02.Conditional-Statements-Advanced/CS-Advanced-Lab/Fruit-Or-Vegetable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FruitOrVegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string item = Console.ReadLine();
+
+            // check if()
+            // switch()
+            //1.input from user
+            //2.check category of the fruid
+            
+
+
+            	//Плодовете "fruit" имат следните възможни стойности:  banana, apple, kiwi, cherry, lemon и grapes
+	            //Зеленчуците "vegetable" имат следните възможни стойности:  tomato, cucumber, pepper и carrot
+	            //Всички останали са "unknown"
+
+
+            switch (item)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+                    Console.WriteLine("fruit");
+                    break;
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    Console.WriteLine("vegetable");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+
+            }
+        }
+    }
+}
