e6cc6fd97ca392d845d8c24df6f863714857071e
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-21 17:03:35 +0200

    Add files via upload

---
+++ 02.Conditional-Statements-Advanced/CS-Advanced-Lab/Animal-Type/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AnimalType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string animal = Console.ReadLine();
+            //crocodile, tortoise, snake -> reptile
+            switch (animal)
+            {
+                case "dog":
+                    Console.WriteLine("mammal");
+                    break;
+                case "crocodile":
+                case "tortoise":
+                case "snake":
+                    Console.WriteLine("reptile");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
