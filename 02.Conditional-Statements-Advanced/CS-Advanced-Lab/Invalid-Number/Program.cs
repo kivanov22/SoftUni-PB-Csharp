72e2d609a797f644c1bf5e4db9d45e30ec4e2612
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-23 10:56:27 +0200

    Add files via upload

---
+++ 02.Conditional-Statements-Advanced/CS-Advanced-Lab/Invalid-Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace InvalidNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.input number int
+            //2.check if its invalid 
+            //3.[100…200] или е 0. 
+            //4. range >=100 , <=200 !=0
+
+            int num = int.Parse(Console.ReadLine());
+
+            if((num >= 100 && num <=200) || num == 0)
+            {
+                
+            }
+            else
+            {
+                Console.WriteLine("invalid");
+            }
+        }
+    }
+}
