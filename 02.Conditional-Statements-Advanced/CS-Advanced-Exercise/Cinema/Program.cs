83137c8e66061d78c24bd05b23ec14db1018d472
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-26 10:46:10 +0200

    Add files via upload

---
+++ 02.Conditional-Statements-Advanced/CS-Advanced-Exercise/Cinema/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	Premiere – премиерна прожекция, на цена 12.00 лева.
+            //•	Normal – стандартна прожекция, на цена 7.50 лева.
+            //•	Discount – прожекция за деца, ученици и студенти на намалена цена от 5.00 лева.
+
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+            double income = 0.0;
+
+            switch (type)
+            {
+                case "Premiere":
+                    income = rows * columns * 12;
+                    break;
+                case "Normal":
+                    income = rows * columns * 7.50;
+                    break;
+                case "Discount":
+                    income = rows * columns * 5;
+                    break;
+            }
+            Console.WriteLine($"{income:f2} leva");
+
+        }
+    }
+}
