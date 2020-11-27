2cdc8f8e902bf04730dcdec0600671486a31a2d4
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-27 13:35:50 +0200

    Add files via upload

---
+++ 03.Loops-Part-1/Loops-one-Lab/Vowels-Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Vowels_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            int Sum = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                switch (text[i])
+                {
+                    case 'a':
+                        Sum += 1;
+                        break;
+                    case 'e':
+                        Sum += 2;
+                        break;
+                    case 'i':
+                        Sum += 3;
+                        break;
+                    case 'o':
+                        Sum += 4;
+                        break;
+                    case 'u':
+                        Sum += 5;
+                        break;
+                }
+
+
+            }
+            Console.WriteLine(Sum);
+        }
+    }
+}
