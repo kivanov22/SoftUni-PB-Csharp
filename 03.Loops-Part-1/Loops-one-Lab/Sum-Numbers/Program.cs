29b18f509cc6314185a6089a7818b1afd723e863
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-27 13:35:04 +0200

    Add files via upload

---
+++ 03.Loops-Part-1/Loops-one-Lab/Sum-Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int Sum = 0;
+
+            for (int i = 0; i < num; i++)
+            {
+                int num2 = int.Parse(Console.ReadLine());
+                Sum = Sum + num2;
+            }
+            Console.WriteLine(Sum);
+        }
+    }
+}
