4495182c7dc01fb53ca64a4acdf409fa4713ffe7
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-27 13:34:15 +0200

    Add files via upload

---
+++ 03.Loops-Part-1/Loops-one-Lab/Odd-Even-Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int evenSum = 0;
+            int oddSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int n1 = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    evenSum += n1;
+                }
+                else
+                {
+                    oddSum += n1;
+                }
+            }
+
+            
+            int totalSum = evenSum + oddSum;
+            if (evenSum == oddSum)
+            {
+
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + evenSum);
+            }
+            else
+            {
+                int diff = Math.Abs(evenSum - oddSum);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + diff);
+            }
+        }
+    }
+}
+
