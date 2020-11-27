58ecb3c7fc9edfa202ac40995bbb0781f2d327f5
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-27 13:31:38 +0200

    Add files via upload

---
+++ 03.Loops-Part-1/Loops-one-Lab/Number-Sequence/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Number_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int big = int.MinValue;
+            int small = int.MaxValue;
+
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < small)
+                {
+                    small = num;
+
+                }
+                if (num > big)
+                {
+                    big = num;
+
+                }
+            }
+            Console.WriteLine($"Max number: {big}");
+            Console.WriteLine($"Min number: {small}");
+           
+            
+        }
+    }
+}
