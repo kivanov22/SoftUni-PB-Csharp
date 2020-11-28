677211ec25144575cd470aae0fc60c44189847fb
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-28 16:39:54 +0200

    Add files via upload

---
+++ 03.Loops-Part-1/Loops-One-Exercise/Salary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= tabs; i++)
+            {
+                string website = Console.ReadLine();
+
+                switch (website)
+                {
+                    case "Facebook":
+                        salary -= 150;
+                        break;
+                    case "Instagram":
+                        salary -= 100;
+                        break;
+                    case "Reddit":
+                        salary -= 100;
+                        break;
+                }
+            
+
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+                break;
+            }
+        }
+                if(salary >0)
+                {
+                    Console.WriteLine(salary);
+                }
+
+            }
+        }
+    }
+
