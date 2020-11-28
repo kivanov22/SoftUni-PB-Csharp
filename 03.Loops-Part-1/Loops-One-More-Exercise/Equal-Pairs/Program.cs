d8a45f8ece7d1efc9587c96ab44da2ca4a96b021
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-28 16:43:23 +0200

    Add files via upload

---
+++ 03.Loops-Part-1/Loops-One-More-Exercise/Equal-Pairs/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Equal_Pairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+
+            int maxDiff = int.MinValue;
+            int sum = 0;
+            int sum2 = 0;
+            int diff = 0;
+
+            for (int i = 1; i <= num1; i++)
+            {
+                sum += int.Parse(Console.ReadLine());
+                sum += int.Parse(Console.ReadLine());
+
+                if (i > 1)
+                {
+                    diff = Math.Abs(sum - sum2);
+                }
+                if (diff > maxDiff)
+                {
+                    maxDiff = diff;
+                }
+                sum2 = sum;
+                sum = 0;
+            }
+            if (maxDiff == 0)
+            {
+                Console.WriteLine($"Yes, value = {sum2}");
+            }
+            else
+            {
+                Console.WriteLine($"No, maxdiff = {maxDiff}");
+            }
+        }
+    }
+
+}
+
