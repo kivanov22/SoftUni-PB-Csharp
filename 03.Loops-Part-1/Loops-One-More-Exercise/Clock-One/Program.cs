054e278d634a30ed33479eb5cfc65f53d7e7358e
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-28 16:42:45 +0200

    Add files via upload

---
+++ 03.Loops-Part-1/Loops-One-More-Exercise/Clock-One/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Clock_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double hour = 0.0;
+            double minute = 23.59;
+
+
+            do
+            {
+                Console.WriteLine($"{hour}:{minute}");
+            } while (true);
+        }
+    }
+}
