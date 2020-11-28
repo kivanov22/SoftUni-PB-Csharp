e1cf744b0a8f6587c3888116a6c1dbe7a72aa1d2
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-28 16:38:38 +0200

    Add files via upload

---
+++ 03.Loops-Part-1/Loops-One-Exercise/Numbers-Ending-7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Numbers_Ending_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for (int i = 7; i <= 997; i++)//startirame ot 7 do 997
+            {
+                if (i % 10 == 7)//modulno delenie na 10 i ravno na 7
+                {
+                    Console.WriteLine(i);//otpechatvame
+                }
+            }
+        }
+    }
+}
