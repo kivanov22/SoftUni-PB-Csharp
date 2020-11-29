c2b1ba32313d8067cce34829d4bf09761fe66b0b
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-29 15:17:02 +0200

    Add files via upload

---
+++ 03.Loops-Part-1/Loops-One-More-Exercise/Hospital/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = int.Parse(Console.ReadLine());
+
+            // 7 lekari po edin pacient na den
+            // vseki treti den se presmqta ako broq na nepregledanite e po > ot pregledanite + 1 lekar -- proverka
+
+            int treated = 0;
+            int untreated = 0;
+            int doctors = 7;
+
+            for (int days = 1; days <= period; days++)
+            {
+                int patients = int.Parse(Console.ReadLine());
+
+                if (days % 3 == 0 && untreated > treated)
+                {
+                    doctors++;
+                }
+                if (patients <= doctors)
+                {
+                    treated += patients;
+                }
+
+                else
+                {
+                    untreated += patients - doctors;
+                    treated += doctors;
+                }
+            }
+            Console.WriteLine($"Treated patients: {treated}. ");
+            Console.WriteLine($"Untreated patients: {untreated}. ");
+
+        }
+    }
+}
