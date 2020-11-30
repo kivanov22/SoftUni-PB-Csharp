e6b3c26f1cb173b04be697531dcf11021d353720
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-30 16:41:12 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Loops-Two-Lab/Graduation-Two/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Graduation_Two
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+
+            double totalSum = 0;
+            double average = 0;
+            int countClases = 1;
+            int countGrades = 0;
+            int fails = 0;
+
+
+            while (countClases <= 12) //cikul ot purvi do 12 klas
+            {
+                double marks = double.Parse(Console.ReadLine());
+
+                if (marks >= 4.00) //dobri ocenki
+                {
+                    countClases++;  //broqch klasove
+                    totalSum += marks; //suma ocenki
+                    countGrades++; //broqch na ocenki kolko sa
+
+                }
+                else //proverka za skusvaniq 
+                {
+                    fails++;
+                    if (fails == 2)
+                    {
+                        Console.WriteLine($"{name} has been excluded at {countClases} grade");
+                        break;
+                    }
+                }
+
+
+
+            }
+            if (countClases >= 12)
+            {
+                average = totalSum / countGrades;
+                Console.WriteLine($"{name} graduated. Average grade: {average:f2}");
+            }
+        }
+    }
+}
+
+
+
