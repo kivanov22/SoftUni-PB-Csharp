be83881f986f7448e758afb710669f33df7bdc5a
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-30 16:45:12 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Loops-Two-Lab/Sequence 2k+1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sequence_2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int k = 1; // broqch
+
+            while (k<=n) // da se izpulnqva dokato k e po malko ot vuvedenoto ot konzolata chislo
+            {
+                Console.WriteLine(k);
+                k = k * 2 + 1; 
+            }
+        }
+    }
+}
