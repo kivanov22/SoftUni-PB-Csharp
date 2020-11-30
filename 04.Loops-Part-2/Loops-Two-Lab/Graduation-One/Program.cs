31e1212973ac8cc9cf3dff9eb4c85ef7ea402ae1
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-30 16:40:30 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Loops-Two-Lab/Graduation-One/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Graduation_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //input
+            string name = Console.ReadLine();
+            //double grades = double.Parse(Console.ReadLine());
+
+            double count = 0;
+            double average = 0;
+            double totalSum = 0;
+          
+
+            //all grades sum and devide to number of grades 
+            //12 grades
+
+            while (count < 12)
+            {
+                double grades = double.Parse(Console.ReadLine());
+
+                if (grades >= 4.00)
+                {
+                    totalSum += grades;
+                    count++;
+
+                }
+            }
+            average = totalSum / 12;
+                Console.WriteLine($"{name} graduated. Average grade: {average:f2}");
+
+            }
+        }
+    }
+
