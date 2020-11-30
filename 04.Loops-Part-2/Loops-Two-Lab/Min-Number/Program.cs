4c1129815298de73eab70240ac08fa15eae106d2
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-30 16:42:34 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Loops-Two-Lab/Min-Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Min_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            int minNumber = int.MaxValue;
+            int k = 0;
+
+            while (k != numbers)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                if (k == numbers)//prekusvame cikula ako broq na chisla k e po golqm ot purvonachalniq zadaden broi ot konzolata
+                {
+
+                    break; // spirane na cikula
+                }
+
+
+                if (n < minNumber) // namirame nai golqmoto chislo
+                {
+                    minNumber = n;
+
+                }
+
+
+                k++; //broqch na chisla
+            }
+            Console.WriteLine(minNumber);
+
+        }
+    }
+}
