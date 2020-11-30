3c508094f0a67627f4201a37209fbabb5a3f9b42
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-30 16:39:44 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Loops-Two-Lab/Account Balance/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Account_Balance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int transactions = int.Parse(Console.ReadLine());
+
+            double bankAccount = 0;
+
+            for (int i = 0; i < transactions; i++)
+            {
+                double money = double.Parse(Console.ReadLine());
+
+                if (money < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                bankAccount += money;
+                Console.WriteLine($"Increase: {money:f2}");
+            }
+            Console.WriteLine($"Total: {bankAccount:f2}");
+        }
+    }
+}
