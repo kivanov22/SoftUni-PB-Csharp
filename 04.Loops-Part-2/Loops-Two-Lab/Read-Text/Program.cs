d6f9151e1b0f2c6e5183890bc55f4d1ac6919b17
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-30 16:44:33 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Loops-Two-Lab/Read-Text/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Read_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int words = 0;
+            while (true)
+            {
+                string input = Console.ReadLine();
+               
+                if (input == "Stop")
+                {
+                    break;
+                }
+                words++;
+                
+            }
+            Console.WriteLine(words);
+
+        }
+    }
+}
