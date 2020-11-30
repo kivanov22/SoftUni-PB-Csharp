f1ec037f7e1bbdbd30c9cb9ec3883d280907257a
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-30 16:43:12 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Loops-Two-Lab/Moving/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.read input
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            //calculate free space
+            int freeSpace = width * length * height;
+
+
+            //2.Create while loop - for the moving of the boxes
+            string input = Console.ReadLine();
+
+            while (input != "Done")
+            {
+
+
+                //remove the taken space from the free space
+                freeSpace -= int.Parse(input);
+
+                //3.check if there is no more free space left or free space less than 0
+                if (freeSpace < 0)
+                {
+
+                    Console.WriteLine($"No more free space! You need {Math.Abs(freeSpace)} Cubic meters more.");
+                    break;
+                }
+                input = Console.ReadLine();
+            }
+            if (input == "Done")
+            {
+                Console.WriteLine($"{freeSpace} Cubic meters left.");
+            }
+
+
+
+        }
+    }
+}
