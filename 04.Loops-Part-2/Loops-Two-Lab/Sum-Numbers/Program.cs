4c66327aabdc3a60b0eb4555ae41fcba54d0aa03
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-30 17:15:06 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Loops-Two-Lab/Sum-Numbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create main variable
+            string input = Console.ReadLine();
+            double sum = 0;
+
+            //bool inside loop
+            while (input !="Stop")
+            {
+                sum += double.Parse(input);//preobrazuvame string v double
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
