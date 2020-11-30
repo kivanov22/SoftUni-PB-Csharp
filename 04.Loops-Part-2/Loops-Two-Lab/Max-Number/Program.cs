73aca40b7cb3c730b1e3dab5fe2ac16efc8155c6
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-11-30 16:41:52 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Loops-Two-Lab/Max-Number/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Max_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+
+            int maxNumber = int.MinValue;
+            int k = 0;
+
+            while (k !=numbers)
+            {
+              int n = int.Parse(Console.ReadLine());
+
+                if (k == numbers)//prekusvame cikula ako broq na chisla k e po golqm ot purvonachalniq zadaden broi ot konzolata
+                {
+                    
+                    break; // spirane na cikula
+                }
+
+
+                if (n > maxNumber) // namirame nai golqmoto chislo
+                {
+                    maxNumber = n;
+                   
+                }
+               
+
+                k++; //broqch na chisla
+            }
+            Console.WriteLine(maxNumber);
+
+        }
+    }
+}
