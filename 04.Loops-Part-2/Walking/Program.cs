3d2983e03a0e48468fe21f184b1ddd8731edaecd
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-02 12:56:03 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Walking/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int steps = 0;
+
+
+            while (steps < 10000)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Going home")
+                {
+                    int stepToHome = int.Parse(Console.ReadLine());
+                    steps += stepToHome;
+                    break;
+
+                }
+                else
+                {
+                    int walkedSteps = int.Parse(command);
+                    steps += walkedSteps;
+                }
+
+            }
+            if (steps >= 10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+            }
+            else
+            {
+                Console.WriteLine($"{10000 - steps} more steps to reach goal.");
+            }
+        }
+    }
+}
