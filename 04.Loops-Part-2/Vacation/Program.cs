283e28ed143b9724867cc91f44e4269fd4bb9c6a
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-02 12:55:28 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Vacation/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tripPrice = double.Parse(Console.ReadLine());
+            double availableMoney = double.Parse(Console.ReadLine());
+
+            int countSpendDays = 0; // dni harchi posledovatelno
+            int countTotalDays = 0; // dni koito izvurshva nqkvo deinost
+
+            while (availableMoney <tripPrice)
+            {
+                string action = Console.ReadLine(); // spend or save
+                double price = double.Parse(Console.ReadLine()); //suma
+                countTotalDays++;
+
+
+                if(action == "save") // kogato spestqva
+                {
+                    availableMoney += price;
+                    countSpendDays = 0;
+                }
+                else if(action == "spend")//kogato harchi
+                {
+                    countSpendDays++;
+                    availableMoney -= price;
+                    if(availableMoney < 0)
+                    {
+                        availableMoney = 0;
+                    }
+                }
+                if (countSpendDays == 5) //harchila pet posledovatelni dni sluchai
+                {
+                    Console.WriteLine("You can't save the money.");
+                    Console.WriteLine(countTotalDays);
+                    break;
+
+                }
+            }
+            if(availableMoney >= tripPrice)//kogato parite i stigat
+            {
+                Console.WriteLine($"You saved the money for {countTotalDays} days.");
+
+            }
+
+
+        }
+    }
+}
