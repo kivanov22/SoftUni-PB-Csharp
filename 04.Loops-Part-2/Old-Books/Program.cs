e04908bc06c198a422b5c148238d14d277a556b4
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-02 12:52:26 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Old-Books/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Old_Books
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchedBook = Console.ReadLine();
+            int countTotalBooks = int.Parse(Console.ReadLine());
+
+            int checkedBooks = 0;
+
+            while (countTotalBooks >0)
+            {
+                string title = Console.ReadLine();
+                
+                if(searchedBook == title)
+                {
+                    // if we find the book
+                    Console.WriteLine($"You checked {checkedBooks} books and found it.");
+                    break;
+                }
+                else
+                {
+                    //if we don't find the book
+                    checkedBooks++;
+                }
+
+                countTotalBooks--;
+            }
+            if(countTotalBooks <= 0)
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {checkedBooks} books.");
+                
+            }
+
+        }
+    }
+}
