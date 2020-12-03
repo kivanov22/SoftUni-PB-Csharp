2c092a26f80fde1bf70d4e5c143321d998045f7e
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:41:40 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested Loops - More Exercises/Lucky-Numbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Lucky_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum1 = 0;
+            int sum2 = 0;
+            for (int num1 = 1; num1 <=9; num1++)
+            {
+                for (int num2 = 1; num2 <=9; num2++)
+                {
+                    for (int num3 = 1; num3 <=9; num3++)
+                    {
+                        for (int num4 = 1; num4 <=9; num4++)
+                        {
+                            sum1 = num1 + num2;
+                            sum2 = num3 + num4;
+
+                            if (sum1== sum2)
+                            {
+                                if(n%sum1 == 0)
+                                {
+                                  Console.Write($"{num1}{num2}{num3}{num4} ");
+                                }
+                                
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
