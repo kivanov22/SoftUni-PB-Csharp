f655be0c045d0ab0843cea203f3f35b2405adea0
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:22:39 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested -Loops - Lab/Building/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberFloors = int.Parse(Console.ReadLine());
+            int numberRooms = int.Parse(Console.ReadLine());
+
+            for (int i = numberFloors; i>=1; i--) // цикъл за етажи намаляват
+            {
+               for (int j = 0; j < numberRooms; j++) // за стай се увеличават
+                {
+
+                    if(numberFloors == i) // ако етажите са равни на на последния етаж се пише Л
+                    {
+                        Console.Write($"L{i}{j} ");
+                        
+                    }
+                    else if (i % 2 == 0)// ако се дели на 2 за четно значи е офис
+                    {
+                        Console.Write($"O{i}{j} ");
+                    }
+                    else // иначе е апартамент
+                    {
+                        Console.Write($"A{i}{j} ");
+                    }
+                }
+                Console.WriteLine(""); 
+               
+            }
+
+
+        }
+    }
+}
