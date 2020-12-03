3c5795e4d794baf0228aed2585211b2977c79a16
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:27:38 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested -Loops - Lab/Traveling/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Traveling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            
+            while(destination != "End") // първо условие за енд от конзолата
+            {
+                double budget = double.Parse(Console.ReadLine()); // бюджета
+
+                double savedMoney = double.Parse(Console.ReadLine()); //спестените пари
+                double totalMoneySaved = savedMoney; //нова променлива присвояваща спестените пари
+
+                while (totalMoneySaved < budget) // второ условие бюджета да е по голям от спестените пари
+                {
+                     savedMoney = double.Parse(Console.ReadLine());
+                     totalMoneySaved += savedMoney;   
+                }
+                Console.WriteLine($"Going to {destination}!");
+
+
+                destination = Console.ReadLine(); 
+
+
+            }
+        }
+    }
+}
