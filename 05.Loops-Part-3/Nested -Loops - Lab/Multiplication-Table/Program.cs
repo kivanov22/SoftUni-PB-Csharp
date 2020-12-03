31a7552a012d0ccf266541b393a6f8327bac774b
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:26:24 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested -Loops - Lab/Multiplication-Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Multiplication_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int n1 = 1; n1 <= 10; n1++)
+            {
+                for (int n2 = 1; n2 <= 10; n2++)
+                {
+                    int multi = n1 * n2;
+                    Console.WriteLine($"{n1} * {n2} = {multi}");
+                }
+            }
+        }
+    }
+}
