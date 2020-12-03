73a3582a6fc6890a0d23b70ab604a8ade6bd87d1
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:27:03 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested -Loops - Lab/Sum-of-Two-Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = int.Parse(Console.ReadLine());
+            int endingNumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int combinations = 0;
+            bool isFound = false;
+
+
+
+            for (int i = startingNumber; i <= endingNumber; i++)
+            {
+                for (int j = startingNumber; j <= endingNumber; j++)
+                {
+                    combinations++;
+
+                    if (i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combinations} ({i} + {j} = {magicNumber})");
+                        isFound = true;
+                        break;
+                    }
+                }
+                if (isFound)
+                {
+                    break;
+                }
+
+            }
+            if (isFound == false)
+            {
+                Console.WriteLine($"{combinations} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
