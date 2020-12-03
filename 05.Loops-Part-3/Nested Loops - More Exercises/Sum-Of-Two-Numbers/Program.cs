e53baab4e41ccaafb66cd775a10e63b570625ed8
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 11:28:12 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested Loops - More Exercises/Sum-Of-Two-Numbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Sum_Of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magic = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            bool noCom = false;
+
+            for (int i = start; i <= end; i++)
+            {
+                for (int k =start; k <= end; k++)
+                {
+                    counter++;
+                 
+
+                    if (i+k==magic)
+                    {
+                        
+                        Console.WriteLine($"Combination N:{counter} ({i} + {k} = {magic})");
+                        noCom = true;//bool true
+                        return; // retrun instead of break
+                    }
+                    
+                }
+            }
+            if (!noCom)// !
+            {
+                // no bool=false here
+                Console.WriteLine($"{counter} combinations - neither equals {magic}");
+                
+            }
+        }
+    }
+}
