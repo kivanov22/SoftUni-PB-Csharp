5c13ecbeb1cc360bb17cdd4cdb6dad43f8f0ea95
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:40:49 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested Loops - More Exercises/Letters-Combination/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Letters_Combination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char letterA =char.Parse (Console.ReadLine());
+            char letterB = char.Parse(Console.ReadLine());
+            char letterC = char.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (char a = letterA; a <= letterB; a++)
+            {
+                for (char b = letterA; b <=letterB; b++)
+                {
+                    for (char c = letterA; c <= letterB; c++)
+                    {
+                        
+                        if (c !=letterC && a !=letterC && b!=letterC)
+                        {
+                            Console.Write($"{a}{b}{c} ");
+                            counter++;
+                        }
+                        
+                    }
+                }
+                
+            }
+            Console.Write(counter);
+        }
+    }
+}
