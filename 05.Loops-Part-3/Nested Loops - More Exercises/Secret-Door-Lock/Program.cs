53019e946829b94c35189fcdac4775d91d74dba8
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 11:27:36 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested Loops - More Exercises/Secret-Door-Lock/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Secret_Door_Lock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hundreds = int.Parse(Console.ReadLine());
+            int tens = int.Parse(Console.ReadLine());
+            int units = int.Parse(Console.ReadLine());
+
+            for (int a = 2; a <= hundreds; a++)
+            {
+                for (int b = 2; b <= tens; b++)
+                {
+                    for (int c = 2; c <=units; c++)
+                    {
+
+                        if (a % 2 == 0)
+                        {
+                            if (b == 2 || b ==3 ||b==5 || b==7)
+                            {
+                                if (c % 2 == 0)
+                                {
+                                    Console.WriteLine($"{a} {b} {c}");
+                                }
+                            }
+                        }
+
+
+                    }
+                }
+            }
+        }
+    }
+}
