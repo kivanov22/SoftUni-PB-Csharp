f9022cc2de13066d66f79bd5cef6b04be34c2940
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:32:03 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested-Loops-Exercise/Special-Numbers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+
+            for (int thousands = 1; thousands <= 9; thousands++)
+            {
+                for (int hundreds = 1; hundreds <= 9; hundreds++)
+                {
+                    for (int tens = 1; tens <= 9; tens++)
+                    {
+                        for (int units = 1; units <= 9; units++)
+                        {
+                            bool check1 = thousands != 0 && n % thousands == 0;
+                            bool check2 = hundreds != 0 && n % hundreds == 0;
+                            bool check3 = tens != 0 && n % tens == 0;
+                            bool check4 = units != 0 && n % units == 0;
+
+
+                            if(check1 && check2&& check3&& check4)
+                            {
+                                Console.Write($"{thousands}{hundreds}{tens}{units} ");
+                            }
+
+                        }
+                    }
+                }
+
+                
+
+
+
+
+            }
+        }
+    }
+}
