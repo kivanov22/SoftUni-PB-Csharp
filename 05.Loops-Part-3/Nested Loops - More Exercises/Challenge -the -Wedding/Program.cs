dcc0dd8c555b849cae0709a9c57f28582f3d46eb
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:39:00 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested Loops - More Exercises/Challenge -the -Wedding/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Challenge__the__Wedding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int men = int.Parse(Console.ReadLine());
+            int girl = int.Parse(Console.ReadLine());
+            int tables = int.Parse(Console.ReadLine());
+
+            int count = 0;
+            bool tableFUll = false;
+
+            for (int m = 1; m <=men; m++)
+            {
+                for (int g = 1; g <=girl; g++)
+                {
+                    
+                   
+                    if (count== tables)
+                    {
+                        
+                        break;
+                    }
+                    count++;
+                    tableFUll = false;
+                    Console.Write($"({m} <-> {g}) ");
+                }
+                if (tableFUll)
+                {
+                    tableFUll = true;
+                }
+            }
+            
+        }
+    }
+}
