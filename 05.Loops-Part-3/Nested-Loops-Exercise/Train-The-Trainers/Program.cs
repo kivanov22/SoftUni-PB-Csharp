be9f0422530c373257d88d733b77260d313ab37e
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:33:25 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested-Loops-Exercise/Train-The-Trainers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberJury = int.Parse(Console.ReadLine());
+            string presentationName = Console.ReadLine();
+
+            double totalSum = 0;
+            int countGrades = 0;
+
+            while (presentationName !="Finish")
+            {
+                double sum = 0; // променлива за оценки 
+
+                for (int judge = 1; judge <= numberJury; judge++) // цикъл за съдий
+                {
+                    double grade = double.Parse(Console.ReadLine()); 
+
+                    sum += grade;//оценките за категорията
+                    totalSum += grade;// сума всички оценки от всички категорий
+                    countGrades++;  //брояч на оценки
+
+                }
+                double average = sum / numberJury;// средна оценка от категория
+
+                Console.WriteLine($"{presentationName} - {average:f2}.");
+
+                presentationName = Console.ReadLine(); // пак питаме за име за презентация
+            }
+            double averageForAll = totalSum / countGrades; // средна оценка от всички категорий
+            Console.WriteLine($"Student's final assessment is {averageForAll:f2}.");
+        }
+    }
+}
