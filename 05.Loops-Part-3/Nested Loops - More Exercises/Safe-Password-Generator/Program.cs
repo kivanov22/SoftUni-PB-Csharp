96819b685d674e466416db18779c9b1c13d05ff6
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:45:09 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested Loops - More Exercises/Safe-Password-Generator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Safe_Password_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int maxPassword = int.Parse(Console.ReadLine());
+
+            int counter = 0;//брояч
+            int i = 35;
+            int j = 64;
+
+            while (i<=55)//първите две букви
+            {
+                while (j<=96)
+                {
+                    for (int k = 1; k <= a; k++)//вторите 2
+                    {
+                        for (int h = 1; h <= b; h++)
+                        {
+
+                            char one = ((char)i);//превръщаме инт в чар
+                            char two = ((char)j); // 
+
+                            Console.Write($"{one}{two}{k}{h}{two}{one}|");// принтираме една комбинация
+                            counter += 1; // брояча го дигаме с 1
+                            i++;
+                            j++;
+
+                            if(counter>=maxPassword || k==a&& h == b)// проверка ако брояча е по голям или равен на макс пароли,
+                            {
+                                return;
+                            }
+                            if (i > 55)// задаваме нови стойности на променивите
+                            {
+                                i = 35;
+                            }
+                            if (j > 96)
+                            {
+                                j = 64;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
