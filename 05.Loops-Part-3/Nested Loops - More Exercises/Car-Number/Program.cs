92ba93e1d35f6a979e516d31205512cc5c78fdcb
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-03 10:35:30 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested Loops - More Exercises/Car-Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Car_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            for (int a = number1; a <= number2; a++)
+            {
+                for (int b = number1; b <=number2; b++)
+                {
+                    for (int c = number1; c <= number2; c++)
+                    {
+                        for (int d = number1; d <= number2; d++)
+                        {
+                            if(a %2==0 && d % 2 != 0 || d%2==0 && a%2!=0)
+                            {
+                                if (a > d)
+                                {
+                                    if ((b + c) % 2 == 0)
+                                    {
+                                        Console.Write($"{a}{b}{c}{d} ");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
