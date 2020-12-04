77bead4558c6cc453a4347f856550da70655ea8f
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-04 14:26:31 +0200

    Add files via upload

---
+++ 04.Loops-Part-2/Loops-two-More-Exercise/Average-Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Average_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double n = int.Parse(Console.ReadLine());
+            double average = 0;
+            double sum = 0;
+            for (int i = 1; i <=n; i++)
+            {
+
+                double number = int.Parse(Console.ReadLine());
+                sum += number;
+                average = sum / n;
+                
+
+            }
+            Console.WriteLine($"{average:f2}");
+
+        }
+    }
+}
