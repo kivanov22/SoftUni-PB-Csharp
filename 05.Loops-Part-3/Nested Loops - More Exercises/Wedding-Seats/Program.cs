0f157d824f5969ab709580dffdc586169827901d
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-04 14:20:28 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested Loops - More Exercises/Wedding-Seats/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Wedding_Seats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char sector = char.Parse(Console.ReadLine());
+            int numberRows = int.Parse(Console.ReadLine());
+            int oddNumberSeats = int.Parse(Console.ReadLine());
+
+            int rowsCount = 0;
+            int sum = 0;
+            int numberEvenSeats = 0;
+
+            for (char i = 'A'; i <=sector; i++)
+            {
+                for (int rows = 1; rows <=numberRows; rows++)
+                {
+                    rowsCount++;
+                    for (char oddSeats = 'a'; oddSeats <='b'; oddSeats++)
+                    {
+                       
+                        for (char evenSeats = 'c'; evenSeats <='d'; evenSeats++)
+                        {
+                            
+                            if (oddNumberSeats % 2 != 0)
+                            {
+                                Console.WriteLine($"{i}{rowsCount}{oddSeats} ");
+                            }
+                            else
+                            {
+                                numberEvenSeats = oddNumberSeats + 2;
+                                Console.WriteLine($"{i}{rowsCount}{evenSeats} ");
+                                
+                            }
+                            
+                            sum = oddNumberSeats + numberEvenSeats;
+                        }
+                    }
+                   
+                }
+            }
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
