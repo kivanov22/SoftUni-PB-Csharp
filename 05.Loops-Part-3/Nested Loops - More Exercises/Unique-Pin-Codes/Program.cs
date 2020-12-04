d94cda6a220470e78b1e6ca91d00e2fb8fc28b87
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-04 14:19:51 +0200

    Add files via upload

---
+++ 05.Loops-Part-3/Nested Loops - More Exercises/Unique-Pin-Codes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Unique_Pin_Codes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+
+            //first and third equal
+            //second has to be simple number
+            for (int start1 = 2; start1 <= num1; start1+=2) // четни числа
+            {
+                for (int start2 = 2; start2 <= num2; start2++)
+                {
+                    for (int start3 = 2; start3 <= num3; start3 += 2)// четни числа
+                    {
+                        if (start2 == 2 || start2 ==3 ||start2 == 5 || start2==7)// прости числа проверка
+                        {
+                            Console.WriteLine($"{start1} {start2} {start3}");
+                        }
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
