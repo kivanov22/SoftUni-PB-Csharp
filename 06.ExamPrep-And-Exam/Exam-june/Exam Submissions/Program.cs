94884aa41921eed8a2d7170023a5f35cfe8f1711
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-07 14:47:33 +0200

    Add files via upload

---
+++ 06.ExamPrep-And-Exam/Exam-june/Exam Submissions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Exam_Submissions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberStudents = int.Parse(Console.ReadLine());
+            double numberExercises = int.Parse(Console.ReadLine());
+            double sumExercise = numberStudents * Math.Ceiling(numberExercises * 2.8);
+            double third = 0;
+            third = numberStudents * Math.Floor(numberExercises / 3);
+            double totalThree = sumExercise + third;
+            double storage = Math.Ceiling(totalThree / 10);
+            double totalAmount = 5 * storage;
+            Console.WriteLine($"{totalAmount} KB needed");
+            Console.WriteLine($"{totalThree} submissions");
+        }
+    }
+}
