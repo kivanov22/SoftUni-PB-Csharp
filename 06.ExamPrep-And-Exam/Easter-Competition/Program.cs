cc4a61ffa3efe58a214e0beb8839c4d20cec8032
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-07 10:57:50 +0200

    Add files via upload

---
+++ 06.ExamPrep-And-Exam/Easter-Competition/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Easter_Competition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberEasterBread = int.Parse(Console.ReadLine());
+             string topChef = "";
+            int maxScore = 0;
+
+            for (int i = 1; i <= numberEasterBread; i++)
+            {
+                string nameBaker = Console.ReadLine();
+                 string score = Console.ReadLine();
+                int sum = 0;
+
+                while (score!="Stop")
+                {
+                    sum += int.Parse(score);
+                    score = Console.ReadLine();
+                }
+                Console.WriteLine($"{nameBaker} has {sum} points.");
+
+                if (sum > maxScore)
+                {
+                    maxScore = sum;
+                    topChef = nameBaker;
+                    Console.WriteLine($"{nameBaker} is the new number 1!");
+                }
+            }
+            Console.WriteLine($"{topChef} won competition with {maxScore} points!");
+
+
+        }
+    }
+}
