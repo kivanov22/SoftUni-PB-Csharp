ff1325ebf8035fbf80ec59fd66a5b6132ea6452a
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-07 10:51:54 +0200

    Add files via upload

---
+++ 06.ExamPrep-And-Exam/Beehive-Population/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Beehive_Population
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //4ta zadacha
+            int startingPopulation = int.Parse(Console.ReadLine());
+            int endYears = int.Parse(Console.ReadLine());
+
+            int newBees = 0;
+            int deadBees = 0;
+            int migBees = 0;
+
+            for (int year = 1; year <= endYears; year++)
+            {
+                 
+                if (year ==5)
+                {
+                    newBees = (startingPopulation / 10) * 2;
+                    startingPopulation += newBees;
+
+                    migBees = (startingPopulation / 50) * 5;
+                    startingPopulation -= migBees;
+
+                    deadBees = (startingPopulation / 20) * 2;
+                    startingPopulation -= deadBees;
+                }
+                else if (startingPopulation >= 10)
+                {
+                    newBees = (startingPopulation / 10) * 2;
+                    startingPopulation += newBees;
+
+                    deadBees = (startingPopulation / 20) * 2;
+                    startingPopulation -= deadBees;
+                }
+            }
+            Console.WriteLine($"Beehive population: {startingPopulation}");
+        }
+    }
+}
