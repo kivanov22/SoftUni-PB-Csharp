73dbccf0f174b2c4fdd5c5af0d039688d2d8a64e
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-07 10:53:28 +0200

    Add files via upload

---
+++ 06.ExamPrep-And-Exam/Honey-Combs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Honey_Combs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberBee = int.Parse(Console.ReadLine());
+            double numberFlowers = int.Parse(Console.ReadLine());
+
+            double totalHoney = numberBee * numberFlowers * 0.21;
+            double howMuch = Math.Floor(totalHoney / 100);
+            double diffHoney = totalHoney - 100*howMuch;
+
+            Console.WriteLine($"{Math.Floor(howMuch)} honeycombs filled.");
+            Console.WriteLine($"{diffHoney:f2} grams of honey left.");
+        }
+    }
+}
