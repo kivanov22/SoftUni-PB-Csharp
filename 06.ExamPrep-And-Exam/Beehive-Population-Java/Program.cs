2921a1c5d1d36ad1fb61cf73ef4680e5df96ae89
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-07 10:56:14 +0200

    Add files via upload

---
+++ 06.ExamPrep-And-Exam/Beehive-Population-Java/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Beehive_Population_Java
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int initialPopulation = int.Parse(Console.ReadLine());
+            int years = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= years; i++)
+            {
+                initialPopulation += initialPopulation / 10 * 2;
+
+                if (i % 5 == 0)
+                {
+                    initialPopulation -= initialPopulation / 50 * 5;
+                }
+                initialPopulation -= initialPopulation / 20 * 2;
+            }
+            Console.WriteLine($"Beehive population: {initialPopulation}");
+        }
+    }
+}
