840d381b84d4fc455bf599f6f36746a94c366f6b
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-07 10:55:34 +0200

    Add files via upload

---
+++ 06.ExamPrep-And-Exam/Beehive-Defence-Java/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Beehive_Defence_Java
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bees = int.Parse(Console.ReadLine());
+            int bearHealth = int.Parse(Console.ReadLine());
+            int bearAtack = int.Parse(Console.ReadLine());
+
+            while (bees>=100)
+            {
+                bees -= bearAtack;
+
+                if (bees <0)
+                {
+                    bees = 0;
+                }
+                bearHealth -= bees * 5;
+                if (bearHealth < 1)
+                {
+                    Console.WriteLine($"Beehive won! Bees left {bees}.");
+                    break;
+                }
+            }
+            if (bees < 100)
+            {
+                Console.WriteLine($"The bear stole the honey! Bees left {bees}.");
+
+            }
+            
+        }
+    }
+}
