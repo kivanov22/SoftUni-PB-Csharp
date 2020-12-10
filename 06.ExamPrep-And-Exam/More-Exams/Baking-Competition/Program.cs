fab6c6810996eec59675e464ea507601f85dad9d
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-10 17:27:18 +0200

    Add files via upload

---
+++ 06.ExamPrep-And-Exam/More-Exams/Baking-Competition/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace Baking_Competition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            int totalGoods = 0;
+            double totalMoney = 0;
+
+            for (int i = 1; i <= numberOfPeople; i++)
+            {
+
+                string name = Console.ReadLine();
+                
+                
+
+                int countCookies = 0;
+                int countWaffles = 0;
+                int countCakes = 0;
+                string input = Console.ReadLine();
+
+                while (input!="Stop baking!")
+                {
+
+                    int numberOfCakes = int.Parse(Console.ReadLine());
+
+                    switch (input)
+                    {
+                        case "cookies":
+                            countCookies += numberOfCakes;
+                            break;
+                        case "waffles":
+                            countWaffles+=numberOfCakes;
+                            break;
+                        case "cakes":
+                            countCakes += numberOfCakes;
+                            break;
+                    }
+
+                    input = Console.ReadLine();
+                }
+                totalGoods += countCakes + countCookies + countWaffles;
+                totalMoney += countCakes * 7.80 + countCookies * 1.50 + countWaffles * 2.30;
+
+
+                Console.WriteLine($"{name} baked {countCookies} cookies, {countCakes} cakes and {countWaffles} waffles.");
+                
+            }
+            Console.WriteLine($"All bakery sold: {totalGoods}");
+            Console.WriteLine($"Total sum for charity: {totalMoney:f2} lv.");
+        }
+    }
+}
