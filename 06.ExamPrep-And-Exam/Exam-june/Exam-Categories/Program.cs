7a38562297b8e1279715222bd136f036bc5d0c74
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-10 17:20:38 +0200

    Add files via upload

---
+++ 06.ExamPrep-And-Exam/Exam-june/Exam-Categories/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int difficulty = int.Parse(Console.ReadLine());
+            int cir = int.Parse(Console.ReadLine());
+
+            int numPages = int.Parse(Console.ReadLine());
+
+            if (difficulty >= 80 && cir >= 80 && numPages >= 8)
+            {
+                Console.WriteLine("Legacy");
+            }
+            else if (difficulty >= 80 && cir <= 10)
+            {
+                Console.WriteLine("Master");
+
+            }
+            else if (difficulty > 0 && cir >= 50 && numPages >= 2)
+            {
+                Console.WriteLine("Hard");
+
+            }
+
+            else if (difficulty <= 30 && numPages <= 1)
+            {
+                Console.WriteLine("Easy");
+
+            }
+            else if (difficulty <= 10)
+            {
+                Console.WriteLine("Elementary");
+
+            }
+            else 
+            {
+                Console.WriteLine("Regular");
+
+            }
+
+        }
+    }
+}
