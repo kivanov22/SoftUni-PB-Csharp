7b8161ee07ffc5b960e92384a3860f3f3856564b
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-10 17:21:26 +0200

    Add files via upload

---
+++ 06.ExamPrep-And-Exam/Exam-june/Exam-Preparation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exam_Preparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int students = int.Parse(Console.ReadLine());
+            int tasks = int.Parse(Console.ReadLine());
+            int trainerEneergy = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < length; i++)
+            {
+
+            }
+
+        }
+    }
+}
