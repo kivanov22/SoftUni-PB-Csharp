312c2a91c890404920524689c2e9e0c8f16bba26
Kristian Ivanov <51948793+kivanov22@users.noreply.github.com>
2020-12-10 17:29:44 +0200

    Add files via upload

---
+++ 06.ExamPrep-And-Exam/More-Exams/Darts-Tournament/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+using System.Drawing;
+
+namespace Darts_Tournament
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingPoints = int.Parse(Console.ReadLine());
+            int count = 0;
+
+
+            while (true)
+            {
+                string sector = Console.ReadLine();
+                count++;
+                
+                if (sector == "bullseye")
+                {
+                    Console.WriteLine($"Congratulations! You won the game with a bullseye in {count} moves!");
+                    break;
+
+                }
+
+                int points = int.Parse(Console.ReadLine());
+
+
+                switch (sector)
+                {
+                    case "number section":
+                        startingPoints -= points;
+                        break;
+                    case "double ring":
+                        points *= 2;
+                        startingPoints -= points;
+
+                        break;
+                    case "triple ring":
+                        points *= 3;
+                        startingPoints -= points;
+                        break;
+                    
+                }
+                
+               
+                
+                if (startingPoints== 0)
+                {
+
+                    Console.WriteLine($"Congratulations! You won the game in {count} moves!");
+                    break;
+                }
+                else if (startingPoints < 0)
+                {
+                    Console.WriteLine($"Sorry, you lost. Score difference: {Math.Abs(startingPoints)}.");
+                    break;
+                }
+            }
+           
+        }
+    }
+}
